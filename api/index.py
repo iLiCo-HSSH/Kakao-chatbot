7a304e3ff486d89fff2811dd01019947947ba52d
janu8ry <janu8ry0108@gmail.com>
2024-03-07 22:30:59 +0900

    add index.html

--- api/index.py
+++ api/index.py
@@ -1,12 +1,8 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def home():
-    return 'Hello, World!'
-
-
-if __name__ == '__main__':
-    app.run()
+    return render_template("index.html")
